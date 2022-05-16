6640b519c12b34d10c1c5bece7c12a019e98261a
hanshao <hanjie@fosunholiday.com>
2022-05-16 15:48:50 +0800

    版本信息

--- examples/main.js
+++ examples/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import VueEleUI from './../packages/index';
-Vue.use(ElementUI)
+// import VueEleUI from './../packages/index';
+import VueEleUI from '../lib/vue-ele-ui.common';
+import '../lib/vue-ele-ui.css';
+Vue.use(ElementUI);
 Vue.use(VueEleUI);
 
 Vue.config.productionTip = false;
