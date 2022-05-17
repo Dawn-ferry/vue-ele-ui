e5317e662301730da80a9e0829f333bf4a2ae16a
hanshao <hanjie@fosunholiday.com>
2022-05-17 18:34:30 +0800

    卡片及目录调整

--- examples/main.js
+++ examples/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// import VueEleUI from './../packages/index';
-import VueEleUI from '../lib/vue-ele-ui.common';
-import '../lib/vue-ele-ui.css';
+import VueEleUI from './../packages/index';
+// import VueEleUI from '../lib/vue-ele-ui.common';
+// import '../lib/vue-ele-ui.css';
 Vue.use(ElementUI);
 Vue.use(VueEleUI);
 
